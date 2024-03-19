1f785998e021eafb3ce98b4853878705c3d5a212
praty474 <p.kayastha.pk@gmail.com>
2024-03-19 21:38:43 +0545

    second

--- movie_app/lib/HomePage/homepage.dart
+++ movie_app/lib/HomePage/homepage.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:js_interop';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:movie_app/apilinks/allapi.dart';
@@ -10,8 +14,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<void> trendingListhome() async {
-    var trendingweekresponse = await http.get(Uri.parse(trendingweekurl));
+  List<Map<String, dynamic>> trendingList = [];
+  Future<void> trendingListHome() async {
+    var trendingWeekResponse = await http.get(Uri.parse(trendingweekurl));
+    if (trendingWeekResponse.statusCode == 200) {
+      var tempData = jsonDecode(trendingWeekResponse.body);
+      var trendingWeekJson = tempData['results'];
+      for (var i = 0; i < trendingWeekJson.length;) {
+        trendingList.add({
+          'id': trendingWeekJson[i]['id'],
+          'poster_path': trendingWeekJson[i]['poster_path'],
+          'vote_averge': trendingWeekJson[i]['vote_averge'],
+          'media_type': trendingWeekJson[i]['media_type'],
+          'indexNo': i,
+        });
+      }
+    }
   }
 
   @override
@@ -20,6 +38,41 @@ class _HomePageState extends State<HomePage> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            centerTitle: true,
+            toolbarHeight: 60,
+            pinned: true,
+            expandedHeight: MediaQuery.of(context).size.height * 0.5,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              background: FutureBuilder(
+                future: trendingListHome(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return CarouselSlider(
+                      options: CarouselOptions(
+                        viewportFraction: 1,
+                        autoPlay: true,
+                        autoPlayInterval:const  Duration(seconds: 2),
+                        height: MediaQuery.of(context).size.height,
+                      ),
+                      items: trendingList.map((i) {
+                        return Builder(
+                          builder: ((context) {
+                            return GestureDetector();
+                          }),
+                        );
+                      }).toList(),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.amber,
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
             title: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
